c79478d5ca5c053e012f1d58c864f2e9f25f818e
ngm <ngmaibulat@gmail.com>
2022-11-24 10:51:29 +0300

    basic version

--- README.md
+++ README.md
@@ -4,5 +4,15 @@ Run a Process with specified ENV vars loaded
 
 ### Use
 
-- create a .env file. You can name it as you prefer and provide the name file CLI option.
-- npx @aibulat/run@latest env
+- create a .env file
+- run: `npx @aibulat/run@latest`
+
+### Examples:
+
+```sh
+npx @aibulat/run@latest ls -la
+npx @aibulat/run@latest cat package.json
+npx @aibulat/run@latest mysql -uroot -p
+npx @aibulat/run@latest bash
+npx @aibulat/run@latest node somescript.js
+```
--- src/run.ts
+++ src/run.ts
@@ -1,3 +1,35 @@
+import child_process from "node:child_process";
 import * as dotenv from "dotenv";
+dotenv.config();
 
-console.log("run with env");
+// console.log(process.env);
+// console.log(process.argv);
+// console.log(process.argv.slice(2));
+
+if (process.argv.length < 3) {
+    console.error("Please provide command to run");
+    process.exit(1);
+}
+
+const program = process.argv[2];
+const args = process.argv.slice(3);
+const spawn = child_process.spawn;
+
+const cmd = spawn(program, args, { stdio: "inherit" });
+
+// cmd.stdout.pipe(process.stdout);
+
+// process.stdin.resume();
+// process.stdin.pipe(cmd.stdin);
+
+// cmd.stdout.on("data", (data) => {
+//     console.log(`stdout: ${data}`);
+// });
+
+// cmd.stderr.on("data", (data) => {
+//     console.error(`stderr: ${data}`);
+// });
+
+// cmd.on("close", (code) => {
+//     console.log(`child process exited with code ${code}`);
+// });
